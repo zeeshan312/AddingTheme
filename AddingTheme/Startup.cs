b04293095b99510446a4eadb8ac744f563eef256
whinstone1 <zeemurrawat@hotmail.com>
2018-04-03 12:29:00 +0500

    Add project files.

---
+++ AddingTheme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AddingTheme.Startup))]
+namespace AddingTheme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
